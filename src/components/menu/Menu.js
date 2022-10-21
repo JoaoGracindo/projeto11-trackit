7b8e7b448cd22a31eea700a1b0bf8d97725a3cf4
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-10-21 19:57:25 -0300

    menu and header

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,14 +6,23 @@ export default function Menu({}) {
     <StyledMenu>
       <div className="header">
         <img className="logo" src={`${logo}`} />
-        <img className="userImg" src="https://www.rbsdirect.com.br/imagesrc/25516126.jpg?w=700" />
+        <img
+          className="userImg"
+          src="https://www.rbsdirect.com.br/imagesrc/25516126.jpg?w=700"
+        />
       </div>
-      <div className="footer"></div>
+      <div className="footer">
+        <span>Hábitos</span>
+        <div>Hoje</div>
+        <span>Histórico</span>
+      </div>
     </StyledMenu>
   );
 }
 
 const StyledMenu = styled.div`
+  z-index: 1;
+
   img {
     height: 50px;
     width: 50px;
@@ -23,18 +32,35 @@ const StyledMenu = styled.div`
     position: fixed;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     bottom: 0;
     left: 0;
-    background-color: #126ba5;
+    background-color: #ffffff;
     width: 100%;
     height: 70px;
+    color: #52b6ff;
+
+    div {
+      width: 91px;
+      height: 91px;
+      color: #ffffff;
+      background-color: #52b6ff;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 10%;
+      font-weight: 400;
+      font-size: 17.976px;
+      line-height: 22px;
+    }
   }
 
   .header {
     position: fixed;
     display: flex;
     justify-content: space-between;
-    align-items:center;
+    align-items: center;
     top: 0;
     left: 0;
     background-color: #126ba5;
@@ -44,17 +70,22 @@ const StyledMenu = styled.div`
   }
 
   .logo {
-    height:48px;
-    width:96px;
-    margin-left:5%;
-
+    height: 48px;
+    width: 96px;
+    margin-left: 5%;
   }
 
   .userImg {
-    width:50px;
-    height:50px;
-    border-radius:50%;
-    margin-right:5%;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 5%;
+  }
 
+  span {
+    font-weight: 400;
+    font-size: 17.976px;
+    line-height: 22px;
+    margin: 0 10%;
   }
 `;
