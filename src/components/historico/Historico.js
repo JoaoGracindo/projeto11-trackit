4c43299d788080dd2308d9c526a88b8cd465ce16
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-10-22 01:48:10 -0300

    final page

--- src/components/historico/Historico.js
+++ src/components/historico/Historico.js
@@ -1,5 +1,44 @@
+import Menu from "./../menu/Menu";
+import styled from "styled-components";
+
 export default function Historico({}) {
-    return(
-        <></>
-    )
+  return (
+    <>
+      <Menu />
+      <StyledHistorico>
+        <header>Histórico</header>
+        <h2>Em breve você poderá ver o histórico dos seus hábitos aqui!</h2>
+      </StyledHistorico>
+    </>
+  );
 }
+
+const StyledHistorico = styled.div`
+  background-color: #e5e5e5;
+  position: fixed;
+  top: 70px;
+  bottom: 70px;
+  width: 100%;
+  z-index: -1;
+
+  h2 {
+    font-weight: 400;
+    font-size: 17.976px;
+    line-height: 22px;
+    margin-left:5%;
+    margin-right:5%;
+
+    color: #666666;
+  }
+
+  header {
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126ba5;
+    margin-top:28px;
+    margin-bottom:17px;
+    margin-left:5%;
+    margin-right:5%;
+  }
+`;
--- src/components/hoje/Hoje.js
+++ src/components/hoje/Hoje.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import StyledHoje from "./StyledHoje";
 import Menu from "../menu/Menu";
 
 export default function Hoje({}) {
@@ -25,74 +25,3 @@ export default function Hoje({}) {
   );
 }
 
-const StyledHoje = styled.div`
-  background-color: #e5e5e5;
-  position: fixed;
-  top: 70px;
-  bottom: 70px;
-  width: 100%;
-  z-index: -1;
-
-  .header {
-    margin-top: 28px;
-    margin-bottom: 28px;
-    margin-left:5%;
-
-    h1 {
-      font-weight: 400;
-      font-size: 22.976px;
-      line-height: 29px;
-      color: #126ba5;
-      margin-bottom:2px;
-    }
-
-    h2 {
-      font-weight: 400;
-      font-size: 17.976px;
-      line-height: 22px;
-
-      color: #bababa;
-    }
-
-  }
-
-  .habito {
-    width: 90%;
-    height: 94px;
-    background-color: #ffffff;
-    border-radius: 5px;
-    margin: 0 auto;
-    position: relative;
-
-    h1 {
-      font-weight: 400;
-      font-size: 19.976px;
-      line-height: 25px;
-      color: #666666;
-      position: absolute;
-      top: 13px;
-      left: 5%;
-    }
-
-    div{
-      position:absolute;
-      bottom:17px;
-      left:5%;
-      font-weight: 400;
-      font-size: 12.976px;
-      line-height: 16px;
-
-      color: #666666;
-    }
-
-    ion-icon {
-      font-size:75px;
-      color: #ebebeb;
-      border-radius: 5px;
-      position: absolute;
-      top: 13px;
-      right: 5%;
-    }
-
-  }
-`;
